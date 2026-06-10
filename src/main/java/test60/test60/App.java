be9a3cabf09084beb6f7e6d826aaba53c1548d41
JorgePerez-dev <jperezaranaz@gmail.com>
2026-06-10 17:04:59 +0200

    prueba tests

--- src/main/java/test60/test60/App.java
+++ src/main/java/test60/test60/App.java
@@ -10,4 +10,27 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    
+    public static boolean esPalindromo(String texto) {
+
+        texto = texto.toLowerCase();
+        texto = texto.replace(" ", "");
+
+        int izquierda = 0;
+        int derecha = texto.length() - 1;
+
+        while (izquierda < derecha) {
+
+            if (texto.charAt(izquierda) != texto.charAt(derecha)) {
+                return false;
+            }
+
+            izquierda++;
+            derecha--;
+        }
+
+        return true;
+    }
+    
+    
 }
