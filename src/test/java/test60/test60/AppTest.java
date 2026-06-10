64c16e1d46a77043a591c4a9cb43d99608a2974d
JorgePerez-dev <jperezaranaz@gmail.com>
2026-06-10 17:14:45 +0200

    pruebaTest

--- src/test/java/test60/test60/AppTest.java
+++ src/test/java/test60/test60/AppTest.java
@@ -1,61 +1,32 @@
 package test60.test60;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
-
 
 public class AppTest {
 
-
-
     @Test
-
     void casoCorrecto1() {
-
         assertTrue(App.esPalindromo("radar"));
-
     }
 
-
-
     @Test
-
     void casoCorrecto2() {
-
         assertFalse(App.esPalindromo("java"));
-
     }
 
-
-
     @Test
-
     void casoIncorrecto1() {
-
         assertTrue(App.esPalindromo("Radar"));
-
     }
 
-
-
     @Test
-
     void casoIncorrecto2() {
-
         assertTrue(App.esPalindromo("Dabale arroz a la zorra el abad"));
-
     }
-
-
 
     @Test
-
     void casoLimite() {
-
-    	assertTrue(App.esPalindromo("")); // o assertFalse según corresponda
-
+        assertTrue(App.esPalindromo(""));
     }
-
 }
